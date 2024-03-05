83c6bc34b8cea6a1aaa8f9b77cbd54383455f516
Mikhail Skliar <skliarmikhail@gmail.com>
2024-03-05 12:26:10 +0300

    Run rubocop after generators

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,10 @@ module SimpleBoardApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators.after_generate do |files|
+      parsable_files = files.filter { |file| file.end_with?('.rb') }
+      system("bundle exec rubocop -A --fail-level=E #{parsable_files.shelljoin}", exception: true)
+    end
   end
 end
